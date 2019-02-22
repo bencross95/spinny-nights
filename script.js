e629a012a2be38f73af296dfec88e56f7db6989e
Ben Cross <ben_cross@me.com>
2019-02-22 00:16:14 +0000

    lil mvp

---
+++ script.js
@@ -0,0 +1,74 @@
+document.getElementById("article-1").addEventListener("mouseover", function() {
+  document.body.style.backgroundImage = "url(images/07.png)";
+  document.body.style.backgroundColor = "#000";
+  document.getElementById("article-1").style.color = "#FFFFFF";
+  document.getElementById("date-1").style.color = "#FFFFFF";
+  document.getElementById("event-name-1").style.color = "#FFFFFF";
+  document.getElementById("manicule-1").style.color = "#FFFFFF";
+  document.getElementById("article-1-ticket").style.color = "#FFFFFF";
+
+}, false);
+
+
+document.getElementById("article-1").addEventListener("mouseout", function() {
+   document.body.style.backgroundImage = "";
+  document.getElementById("article-1").style.color = ""
+  document.body.style.backgroundColor = "";
+  document.getElementById("date-1").style.color = "";
+  document.getElementById("event-name-1").style.color = "";
+  document.getElementById("manicule-1").style.color = "";
+  document.getElementById("article-1-ticket").style.color = "";
+}, false);
+
+
+
+// ah man
+
+
+document.getElementById("article-2").addEventListener("mouseover", function() {
+  document.body.style.backgroundImage = "url(images/01.png)";
+  document.body.style.backgroundColor = "#000";
+  document.getElementById("article-2").style.color = "#FFFFFF";
+  document.getElementById("date-2").style.color = "#FFFFFF";
+  document.getElementById("event-name-2").style.color = "#FFFFFF";
+  document.getElementById("manicule-2").style.color = "#FFFFFF";
+  document.getElementById("article-2-ticket").style.color = "#FFFFFF";
+
+}, false);
+
+
+document.getElementById("article-2").addEventListener("mouseout", function() {
+  document.body.style.backgroundImage = "";
+  document.getElementById("article-2").style.color = ""
+  document.body.style.backgroundColor = "";
+  document.getElementById("date-2").style.color = "";
+  document.getElementById("event-name-2").style.color = "";
+  document.getElementById("manicule-2").style.color = "";
+  document.getElementById("article-2-ticket").style.color = "";
+}, false);
+
+
+// ah boooi
+
+
+document.getElementById("article-3").addEventListener("mouseover", function() {
+  document.body.style.backgroundImage = "url(images/11.png)";
+  document.body.style.backgroundColor = "#000";
+  document.getElementById("article-3").style.color = "#FFFFFF";
+  document.getElementById("date-3").style.color = "#FFFFFF";
+  document.getElementById("event-name-3").style.color = "#FFFFFF";
+  document.getElementById("manicule-3").style.color = "#FFFFFF";
+  document.getElementById("article-3-ticket").style.color = "#FFFFFF";
+
+}, false);
+
+
+document.getElementById("article-3").addEventListener("mouseout", function() {
+  document.body.style.backgroundImage = "";
+  document.getElementById("article-3").style.color = ""
+  document.body.style.backgroundColor = "";
+  document.getElementById("date-3").style.color = "";
+  document.getElementById("event-name-3").style.color = "";
+  document.getElementById("manicule-3").style.color = "";
+  document.getElementById("article-3-ticket").style.color = "";
+}, false);
