fdee435ebac88eca3152ba9df41d23e79e16211f
Ben Cross <ben_cross@me.com>
2019-02-22 16:36:57 +0000

    cleaned up the hover logic

--- script.js
+++ script.js
@@ -1,74 +1,23 @@
-document.getElementById("article-1").addEventListener("mouseover", function() {
-  document.body.style.backgroundImage = "url(images/07.png)";
-  document.body.style.backgroundColor = "#000";
-  document.getElementById("article-1").style.color = "#FFFFFF";
-  document.getElementById("date-1").style.color = "#FFFFFF";
-  document.getElementById("event-name-1").style.color = "#FFFFFF";
-  document.getElementById("manicule-1").style.color = "#FFFFFF";
-  document.getElementById("article-1-ticket").style.color = "#FFFFFF";
+imagesArray = [
+  'images/01.png',
+  'images/02.png',
+  'images/03.png'
+]
 
-}, false);
+function imageHover(index) {
+  console.log(index);
+  document.body.style.backgroundImage = `url(${imagesArray[index]})`;
+  document.getElementById(`date-${index}`).style.color = "#FFFFFF";
+  document.getElementById(`event-name-${index}`).style.color = "#FFFFFF";
+  document.getElementById(`manicule-${index}`).style.color = "#FFFFFF";
+  document.getElementById(`article-ticket-${index}`).style.color = "#FFFFFF";
+  console.log(imagesArray[index]);
+}
 
-
-document.getElementById("article-1").addEventListener("mouseout", function() {
-   document.body.style.backgroundImage = "";
-  document.getElementById("article-1").style.color = ""
-  document.body.style.backgroundColor = "";
-  document.getElementById("date-1").style.color = "";
-  document.getElementById("event-name-1").style.color = "";
-  document.getElementById("manicule-1").style.color = "";
-  document.getElementById("article-1-ticket").style.color = "";
-}, false);
-
-
-
-// ah man
-
-
-document.getElementById("article-2").addEventListener("mouseover", function() {
-  document.body.style.backgroundImage = "url(images/01.png)";
-  document.body.style.backgroundColor = "#000";
-  document.getElementById("article-2").style.color = "#FFFFFF";
-  document.getElementById("date-2").style.color = "#FFFFFF";
-  document.getElementById("event-name-2").style.color = "#FFFFFF";
-  document.getElementById("manicule-2").style.color = "#FFFFFF";
-  document.getElementById("article-2-ticket").style.color = "#FFFFFF";
-
-}, false);
-
-
-document.getElementById("article-2").addEventListener("mouseout", function() {
+function imageReset(index) {
   document.body.style.backgroundImage = "";
-  document.getElementById("article-2").style.color = ""
-  document.body.style.backgroundColor = "";
-  document.getElementById("date-2").style.color = "";
-  document.getElementById("event-name-2").style.color = "";
-  document.getElementById("manicule-2").style.color = "";
-  document.getElementById("article-2-ticket").style.color = "";
-}, false);
-
-
-// ah boooi
-
-
-document.getElementById("article-3").addEventListener("mouseover", function() {
-  document.body.style.backgroundImage = "url(images/11.png)";
-  document.body.style.backgroundColor = "#000";
-  document.getElementById("article-3").style.color = "#FFFFFF";
-  document.getElementById("date-3").style.color = "#FFFFFF";
-  document.getElementById("event-name-3").style.color = "#FFFFFF";
-  document.getElementById("manicule-3").style.color = "#FFFFFF";
-  document.getElementById("article-3-ticket").style.color = "#FFFFFF";
-
-}, false);
-
-
-document.getElementById("article-3").addEventListener("mouseout", function() {
-  document.body.style.backgroundImage = "";
-  document.getElementById("article-3").style.color = ""
-  document.body.style.backgroundColor = "";
-  document.getElementById("date-3").style.color = "";
-  document.getElementById("event-name-3").style.color = "";
-  document.getElementById("manicule-3").style.color = "";
-  document.getElementById("article-3-ticket").style.color = "";
-}, false);
+  document.getElementById(`date-${index}`).style.color = "";
+  document.getElementById(`event-name-${index}`).style.color = "";
+  document.getElementById(`manicule-${index}`).style.color = "";
+  document.getElementById(`article-ticket-${index}`).style.color = "";
+}
