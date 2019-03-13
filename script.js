f25a5b5dd78cb987948a8aa5a167b94857532986
Ben Cross <ben_cross@me.com>
2019-03-13 18:22:15 +0000

    updated events

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 imagesArray = [
   'images/01.png',
   'images/02.png',
-  'images/03.png'
+  'images/03.png',
+  'images/04.png',
+  'images/05.png',
+  'images/06.png',
+  'images/07.png',
+  'images/08.png',
+  'images/09.png',
+  'images/10.png',
+  'images/11.png',
 ]
 
 function imageHover(index) {
