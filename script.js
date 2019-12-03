45fdd43c9e8427970ca82bf285ee5d4886229aa8
Ben Cross <ben_cross@me.com>
2019-12-03 18:05:25 +0000

    SN18 now previous

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ imagesArray = [
   'images/nightImages/21.jpg',
   'images/nightImages/22.jpg',
   'images/nightImages/23.jpg',
-  // 'images/nightImages/24.jpg',
+  'images/nightImages/24.jpg',
   // 'images/nightImages/25.jpg',
   // 'images/nightImages/26.jpg',
   // 'images/nightImages/27.jpg',
